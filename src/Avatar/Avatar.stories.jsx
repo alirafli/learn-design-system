d2e5837f4288e91b9bbcf41b539f8ef2af877872
alirafli <mrafli1212@gmail.com>
2024-06-30 19:42:26 +0700

    feat

--- src/Avatar/Avatar.stories.jsx
+++ src/Avatar/Avatar.stories.jsx
@@ -3,26 +3,90 @@ import { Avatar } from './Avatar';
 export default {
   title: 'Design System/Avatar',
   component: Avatar,
-  /*
-   * More on Storybook argTypes at:
-   * https://storybook.js.org/docs/react/api/argtypes
-   */
-  argTypes: {
-    size: {
-      control: {
-        type: 'select',
-      },
-      options: ['tiny', 'small', 'medium', 'large', 'dsadsa'],
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      subtitle: 'Displays an image that represents a user or organization',
     },
   },
 };
 
-// Other Avatar stories
+export const Standard = {
+  args: {
+    size: 'large',
+    username: 'Tom Coleman',
+    src: 'https://avatars2.githubusercontent.com/u/132554',
+  },
+};
 
-/*
- * New story using Controls
- * Read more about Storybook templates at:
- * https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+/**
+ * 4 sizes are supported.
+ */
+export const Sizes = {
+  args: {
+    username: 'Tom Coleman',
+    src: 'https://avatars2.githubusercontent.com/u/132554',
+  },
+  render: (args) => (
+    <>
+      <Avatar {...args} size='large' />
+      <Avatar {...args} size='medium' />
+      <Avatar {...args} size='small' />
+      <Avatar {...args} size='tiny' />
+    </>
+  ),
+};
+
+/**
+ * Shows the user's initials as a fallback when no image is provided.
+ */
+export const Initials = {
+  render: (args) => (
+    <>
+      <Avatar username='Tom Coleman' />
+      <Avatar username='Dominic Nguyen' />
+      <Avatar username='Varun Vachhar' />
+      <Avatar username='Michael Shilman' />
+    </>
+  ),
+};
+
+/**
+ * Shows a loading indicator.
+ */
+export const Loading = {
+  args: {
+    loading: true,
+  },
+  render: (args) => (
+    <>
+      <Avatar {...args} size='large' />
+      <Avatar {...args} size='medium' />
+      <Avatar {...args} size='small' />
+      <Avatar {...args} size='tiny' />
+    </>
+  ),
+};
+
+/**
+ * Shows the user's avatar when provided with a `src` prop or in various states and sizes.
+ */
+export const Large = {
+  render: () => (
+    <>
+      <Avatar loading size='large' />
+      <Avatar size='large' username='Tom Coleman' />
+      <Avatar
+        size='large'
+        username='Tom Coleman'
+        src='https://avatars2.githubusercontent.com/u/132554'
+      />
+    </>
+  ),
+};
+
+/**
+ * Avatar component using Controls
  */
 export const Controls = {
   args: {
